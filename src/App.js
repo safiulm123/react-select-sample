11d69ff51463fbf51544d57959c5b85906512ded
Safiul Hasan <safiulhasan@gmail.com>
2019-03-18 12:13:51 +0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Select from 'react-select';
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+];
+
+class App extends Component {
+  state = {
+    selectedOption: null,
+  }
+  handleChange = (selectedOption) => {
+    this.setState({ selectedOption });
+    console.log(`Option selected:`, selectedOption);
+  }
+  render() {
+    const { selectedOption } = this.state;
+
+    return (
+      <Select
+        value={selectedOption}
+        onChange={this.handleChange}
+        options={options}
+        isSearchable={true}
+      />
+    );
+  }
+}
+
+export default App;
